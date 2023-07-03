cd8132b38d927c2d9664618004b0c87c19a01c7a
ussal <62699112+ussal@users.noreply.github.com>
2023-07-03 15:36:49 +0300

    Proje dosyaları eklendi - obj klasörü hariç

---
+++ Net6AppsettingsConfiguration/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Net6AppsettingsConfiguration.Helpers;
+using Net6AppsettingsConfiguration.Models;
+using System.Diagnostics;
+
+namespace Net6AppsettingsConfiguration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //Getting configuration string
+            var configText = ConfigurationHelper.GetConfigurationString("Application:configText");
+            //Send data to View
+            ViewData["configText"] = configText;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Net6AppsettingsConfiguration/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace Net6AppsettingsConfiguration.Helpers
+{
+    public class ConfigurationHelper
+    {
+        private static IConfiguration _config;
+
+        public static void Configure(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public static string GetConfigurationString(string Path)
+        {
+            return _config[Path];
+        }
+    }
+}
